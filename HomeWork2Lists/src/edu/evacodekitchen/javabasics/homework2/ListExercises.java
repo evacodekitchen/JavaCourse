d1ca294fa99e3ae7b871dc05a570aace4205dcb8
evacodekitchen <evacodekitchen@gmail.com>
2021-02-24 13:18:30 +0100

    list exercises

---
+++ HomeWork2Lists/src/edu/evacodekitchen/javabasics/homework2/ListExercises.java
@@ -0,0 +1,36 @@
+package edu.evacodekitchen.javabasics.homework2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListExercises {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(7, 1, 8, 3, -4, 9);
+		System.out.println("min: " + findMin(numbers));
+		System.out.println("max: " + findMax(numbers));
+		System.out.println("odd numbers: " + findOddNumbers(numbers));
+		System.out.println("sum of elements: " + sumOfElements(numbers));
+	}
+
+	public static int findMin(List<Integer> numbers) {
+		// TODO
+		return 0;
+	}
+
+	public static int findMax(List<Integer> numbers) {
+		// TODO
+		return 0;
+	}
+
+	public static List<Integer> findOddNumbers(List<Integer> numbers) {
+		// TODO
+		return null;
+	}
+
+	public static Integer sumOfElements(List<Integer> numbers) {
+		// TODO
+		return null;
+	}
+
+}
